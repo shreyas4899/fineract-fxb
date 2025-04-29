328bb7c72e1512b9b929d3ab7335ebdcfd6b5464
Adam Saghy <adamsaghy@gmail.com>
2025-04-29 13:57:31 +0200

    JDK17 amendments

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/LoanTransactionProcessingServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/LoanTransactionProcessingServiceImpl.java
@@ -121,9 +121,9 @@ public class LoanTransactionProcessingServiceImpl implements LoanTransactionProc
     private Loan getLoan(List<LoanTransaction> loanTransactions, List<LoanRepaymentScheduleInstallment> installments,
             Set<LoanCharge> charges) {
         if (!ObjectUtils.isEmpty(loanTransactions)) {
-            return loanTransactions.getFirst().getLoan();
+            return loanTransactions.get(0).getLoan();
         } else if (!ObjectUtils.isEmpty(installments)) {
-            return installments.getFirst().getLoan();
+            return installments.get(0).getLoan();
         } else if (!ObjectUtils.isEmpty(charges)) {
             return charges.iterator().next().getLoan();
         } else {
